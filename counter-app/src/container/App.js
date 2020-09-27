6fab7a3ff8bcc8ccabba91e8155317857627b8b6
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2020-09-27 22:52:51 +0530

    updated branch

--- counter-app/src/container/App.js
+++ counter-app/src/container/App.js
@@ -1,46 +1,43 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "../features/counter/counter";
-// import { increment, decrement } from "../features/counter/counterSlice";
 import { Box, Card, Heading, Button, Flex } from "rebass";
 
-class App extends Component {
-  render() {
-    return (
-      <Flex justifyContent={"center"}>
-        <Box width={256} mt={10}>
-          <Card
-            sx={{
-              p: 1,
-              borderRadius: 2,
-              boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
-              textAlign: "center",
-            }}
-          >
-            <Box px={2}>
-              <Heading as="h1">{this.props.value}</Heading>
-              <Button
-                sx={{ color: "#000" }}
-                variant="primary"
-                mr={2}
-                onClick={this.props.increment}
-              >
-                add
-              </Button>
-              <Button sx={{ color: "#000" }} onClick={this.props.decrement}>
-                decre
-              </Button>
-            </Box>
-          </Card>
-        </Box>
-      </Flex>
-    );
-  }
-}
+const App = () => {
+  const value = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+  return (
+    <Flex justifyContent={"center"}>
+      <Box width={256} mt={10}>
+        <Card
+          sx={{
+            p: 1,
+            borderRadius: 2,
+            boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
+            textAlign: "center",
+          }}
+        >
+          <Box px={2}>
+            <Heading as="h1">{value}</Heading>
+            <Button
+              sx={{ color: "#000" }}
+              variant="primary"
+              mr={2}
+              onClick={() => dispatch(increment())}
+            >
+              add
+            </Button>
+            <Button
+              sx={{ color: "#000" }}
+              onClick={() => dispatch(decrement())}
+            >
+              decre
+            </Button>
+          </Box>
+        </Card>
+      </Box>
+    </Flex>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  value: state.value,
-});
-
-const mapDispatchToProps = { increment, decrement };
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./container/App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import rootReducer from "./features/counter/counterSlice";
+import rootReducer from "./features/counter/counter";
 import "./index.scss";
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
 });
 
 ReactDOM.render(
